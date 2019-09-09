48f1612526b5502c8b5998a05d7372faca35f598
bijanmehr <bijanmehralizadeh@gmail.com>
2019-09-09 23:12:18 +0430

    add get_center callback and a GUI

--- src/aruco_position_holder.py
+++ src/aruco_position_holder.py
@@ -3,27 +3,50 @@
 import rospy
 from std_msgs.msg import String
 import geometry_msgs.msg
-# import sensor_msgs/Image
+from sensor_msgs.msg import sensor_msgs, Image
 import math
+from cv_bridge import CvBridge
+import cv2
+from PID import pid
 
 
-# def get_center():
-#     pass
+bridge = CvBridge()
+
+center = [0,0]
+position = [0,0,0]
+
+def get_center(data):
+    
+    h = data.height/2
+    w = data.width/2
+    global center
+    center = [h,w]
 
 
 def read_position(data):
     
-    robot_x_pos = data.pose.position.x / 10 # centimeter
-    robot_y_pos = data.pose.position.y / 10 # centimeter
-    robot_z_pos = data.pose.position.z / 10 # centimeter
-    position = [robot_x_pos, robot_y_pos, robot_z_pos]
-    print(data)
+    x_pos = data.pose.position.x * 100 # millimeter
+    y_pos = data.pose.position.y * 100 # millimeter
+    z_pos = data.pose.position.z * 100 # millimeter
+    global position
+    position = [x_pos, y_pos, z_pos]
+
+def gui(data):
+    cv2_img = bridge.imgmsg_to_cv2(data, "bgr8")
+    cv2.line(cv2_img, (center[1], 0), (center[1], 2*center[0]), (0, 20, 200), 1)
+    cv2.line(cv2_img, (0, center[0]), (2*center[1], center[0]), (0, 20, 200), 1)
+    cv2.circle(cv2_img, (center[1], center[0]), 5, (0, 20, 200), 1)
+    cv2.imshow('raw_image', cv2_img)
+    cv2.waitKey(1)
 
 def main():
 
     rospy.init_node('position_holder', anonymous=False)
     rospy.Subscriber("/aruco_single/pose", geometry_msgs.msg.PoseStamped, read_position)
-    # rospy.Subscriber("/tello_node/image_raw", sensor_msgs/Image, read_position)
+    rospy.Subscriber("/tello_node/image_raw", sensor_msgs.msg.Image, get_center)
+    rospy.Subscriber("/aruco_single/result", sensor_msgs.msg.Image, gui)
+    # pose_publisher = rospy.Publisher('position', String, queue_size=10)
+    # pose_publisher.publish(position)
     rospy.spin()
 
 if __name__ == '__main__':
